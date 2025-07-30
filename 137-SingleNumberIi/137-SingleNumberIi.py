aa9b9a207d64596b5b3cc087ff0379ad0219cb6b
DEVATS <devats266@gmail.com>
2025-07-30 12:24:36 -0300

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.py
@@ -0,0 +1,9 @@
+# Last updated: 7/30/2025, 12:24:35 PM
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        m = Counter(nums)
+        value = sorted(m.items() , key = lambda i : i[1], reverse = True)
+        return value[-1][0]
+        
+        
