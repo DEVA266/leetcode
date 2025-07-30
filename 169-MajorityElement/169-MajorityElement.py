3fdbee1d23416cf48b609f5653d5816ba78067ef
DEVATS <devats266@gmail.com>
2025-07-30 12:24:31 -0300

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.py
@@ -0,0 +1,9 @@
+# Last updated: 7/30/2025, 12:24:29 PM
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return nums[len(nums) // 2]
