6ea2ceea691637120cb30753c8755942f07dffda
DEVATS <devats266@gmail.com>
2025-07-30 12:22:37 -0300

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,6 @@
+// Last updated: 7/30/2025, 12:22:35 PM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[] {celsius+273.15,celsius*1.80 + 32.00};
+    }
+}
